d4df2c53fff12862c3599b771c133d7b0497d04b
dewi911 <dewimishkamimimi@gmail.com>
2024-07-23 08:17:54 +0300

    config

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,48 @@
 package main
 
 import (
+	"cruda-app/internal/config"
 	"cruda-app/internal/repository/psql"
 	"cruda-app/internal/service"
 	"cruda-app/internal/transport/rest"
 	"cruda-app/pkg/database"
+	"fmt"
+	"github.com/kelseyhightower/envconfig"
 	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"time"
 )
 
+const (
+	CONFIG_DIR  = "configs"
+	CONFIG_FILE = "main"
+)
+
 func main() {
+	cfg, err := config.New(CONFIG_DIR, CONFIG_FILE)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var crftest = new(config.Config)
+
+	if err := envconfig.Process("db", &crftest.DB); err != nil {
+
+		log.Printf("Error processing DB_HOST: %v", err)
+
+	}
+	fmt.Printf("%+v\n", crftest.DB.Host)
+
+	log.Printf("config: %+v\n", crftest.DB)
+
 	db, err := database.NewPostgresConnection(database.ConnectionInfo{
-		Host:     "localhost",
-		Port:     5432,
-		Username: "postgres",
-		DBName:   "postgres",
-		SSLMode:  "disable",
-		Password: "qwerty123",
+		Host:     cfg.DB.Host,
+		Port:     cfg.DB.Port,
+		Username: cfg.DB.Username,
+		DBName:   cfg.DB.Name,
+		SSLMode:  cfg.DB.SSLMode,
+		Password: cfg.DB.Password,
 	})
 	if err != nil {
 		log.Fatal(err)
@@ -30,7 +54,7 @@ func main() {
 	handler := rest.NewHandler(bookService)
 
 	srv := &http.Server{
-		Addr:    ":8080",
+		Addr:    fmt.Sprintf(":%d", cfg.Server.Port),
 		Handler: handler.InitRouter(),
 	}
 
---
+++ internal/config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"github.com/kelseyhightower/envconfig"
+	"github.com/spf13/viper"
+	"log"
+)
+
+type Config struct {
+	DB     Postgres
+	Server struct {
+		Port int `mapstructure:"port"`
+	} `mapstructure:"server"`
+}
+
+type Postgres struct {
+	Host     string
+	Port     int
+	Username string
+	Name     string
+	SSLMode  string
+	Password string
+}
+
+func New(folder, filename string) (*Config, error) {
+	cfg := new(Config)
+	if err := envconfig.Process("DB", &cfg.DB); err != nil {
+		log.Printf("Error processing env variables: %v", err)
+		return nil, err
+	}
+
+	viper.AddConfigPath(folder)
+	viper.SetConfigName(filename)
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	if err := viper.Unmarshal(cfg); err != nil {
+		return nil, err
+	}
+
+	if err := envconfig.Process("DB", &cfg.DB); err != nil {
+		log.Printf("Error processing DB_HOST: %v", err)
+	}
+
+	return cfg, nil
+}
